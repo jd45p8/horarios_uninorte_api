9acce3e16c35569bc5ac5ab3359ccb472453ffe8
José David Polo <polodj@uninorte.edu.co>
2019-07-19 13:15:38 -0500

    Se obtienen los nombres y códigos de los departamentos.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest
 RUN apt update \
     && apt upgrade -y\
-    && apt install curl -y
+    && apt install curl build-essential -y
 RUN curl -sL https://deb.nodesource.com/setup_10.x | bash - \
     && apt install nodejs -y
 RUN npm i
--- routes.js
+++ routes.js
@@ -1,46 +1,102 @@
 const https = require('https')
-const DOMParser = require('dom-parser')
+const cheerio = require('cheerio')
+const Iconv = require('iconv').Iconv
+const querystring = require('querystring')
 
 exports.routes = {
     '/departamentos': {
         methods: {
             GET: (req, res) => {
                 const options = {
-                    hostname: 'www.uninorte.edu.co',
+                    hostname: 'guayacan.uninorte.edu.co',
                     port: 443,
-                    path: '/web/matriculas/horarios',
+                    path: '/registro/consulta_horarios.asp',
                     method: 'GET'
                 }
 
                 const extReq = https.request(options, extRes => {
                     var chunks = []
-                    extRes.on('data', data => {
+                    extRes.on('data', data =>
                         chunks.push(data)
-                    })
+                    )
 
                     extRes.on('end', () => {
-                        const parser = new DOMParser()
-                        const html = Buffer.concat(chunks).toString()
+                        const iconv = new Iconv('ISO-8859-1', 'UTF-8')
+                        const html = iconv.convert(Buffer.concat(chunks))
 
-                        const doc = parser.parseFromString(html, 'text/html')
-                        const body = doc.getElementsByTagName('body')[0]                 
-                        console.log(body.innerHTML)
+                        const $ = cheerio.load(html)
+                        let dep = {}
+                        $('#departamento option').each((i, elem) => {
+                            if (i != 0) {
+                                dep[i - 1] = {
+                                    name: $(elem).text(),
+                                    code: $(elem).attr('value')
+                                }
+                            }
+                        })
 
                         res.statusCode = 200
-                        res.setHeader('Content-type','text/html')
-                        res.end(html)
-                        return                 
+                        res.setHeader('Content-type', 'application/json; charset=utf-8')
+                        res.write(JSON.stringify(dep))
+                        res.end()
+                        return
                     })
                 })
 
-                
+
                 extReq.on('error', error => {
                     res.statusCode = 500
                     res.setHeader('Content-type', 'text/html')
                     res.end('<h1>500, something went wrong!</h1>')
                     return
                 })
-                
+
+                extReq.end()
+            }
+        }
+    },
+    '/asignaturas': {
+        methods: {
+            GET: (req, res) => {
+                const data = {
+                    'departamento': '0047',
+                    'valida': 'OK',
+                    'datos_periodo': '201930',
+                    'nom_periodo': 'Segundo Semestre 2019',
+                    'datos_nivel': 'PR',
+                    'nom_nivel': 'Pregrado',
+                    'BtnNRC': 'Buscar NRC',
+                }
+                const query = querystring.stringify(data)
+
+                const options = {
+                    hostname: 'guayacan.uninorte.edu.co',
+                    port: 443,
+                    path: '/registro/resultado_departamento1.php?' + query,
+                    method: 'GET'
+                }
+
+                const extReq = https.request(options, extRes => {
+                    var chunks = []
+                    extRes.on('data', data =>
+                        chunks.push(data)
+                    )
+
+                    extRes.on('end', () => {
+                        res.statusCode = 200
+                        res.setHeader('Content-type', 'text/html')
+                        res.end(Buffer.concat(chunks).toString())
+                        return
+                    })
+                })
+
+                extReq.on('error', error => {
+                    res.statusCode = 500
+                    res.setHeader('Content-type', 'text/html')
+                    res.end('<h1>500, something went wrong!</h1>')
+                    return
+                })
+
                 extReq.end()
             }
         }
