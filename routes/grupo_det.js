eceb34a194ff65faedeedff9211ea0c2007e52fb
José David Polo <polodj@uninorte.edu.co>
2019-07-21 11:02:16 -0500

    Se añadió un try-catch para evitar el error al traer los detalles de los grupos.

--- routes/grupo_det.js
+++ routes/grupo_det.js
@@ -48,73 +48,79 @@ exports.grupo_det = {
                 })
 
                 extRes.on('end', () => {
-                    const iconv = new Iconv('ISO-8859-1', 'UTF-8')
-                    const html = iconv.convert(Buffer.concat(chunks))
+                    try {
+                        const iconv = new Iconv('ISO-8859-1', 'UTF-8')
+                        const html = iconv.convert(Buffer.concat(chunks))
 
-                    const $ = cheerio.load(html)
-                    const det = {
-                        subject_name: $('body > div > p.msg1').text(),
-                        dep_name: $('body > div > p:nth-child(2)').text(),
-                        det_asign: $('body > div > p:nth-child(3)').text().split('\n\t\t\t'),
-                        cupos: $('body > div > p:nth-child(4)').text().split('\n\t\t\t'),
-                        professors: $('body > div > p.msg5').text().trim().split(':')[1].split('\n\t'),
-                        dates: $('body > div > p.msg3').text().split('\n')
-                    }
-
-                    let grp_det = {
-                        name: det.subject_name,
-                        dep_name: det.dep_name.split(':').slice(1).join(' ').trim(),
-                        subject: det.det_asign[0].split(':')[1].trim(),
-                        level: det.det_asign[3].split(':')[1].trim(),
-                        group_number: det.det_asign[1].split(':')[1].trim(),
-                        cupos: {
-                            registered: det.cupos[0].split(':')[1].trim(),
-                            available: det.cupos[1].split(':')[1].trim()
-                        },
-                        professors: [],
-                        dates: {
-                            start: det.dates[1].split(':')[1],
-                            end: det.dates[2].split(':')[1]
-                        },
-                        schedule: []
-                    }
-
-                    det.professors.map(professor => {
-                        grp_det.professors.push({
-                            first_name: professor.trim().split(',')[1].trim(),
-                            last_name: professor.trim().split(',')[0].trim()
-                        })
-                    })
-
-                    $('#acreditaciones_resultado > div > div > table > tbody tr').each((i, elem) => {
-                        if (i != 0) {
-                            const row = $(elem).text().trim().split('\n')
-                            let new_item = {
-                                day: row[2].trim(),
-                                interval: {
-                                    start: row[3].split('-')[0].trim(),
-                                    end: row[3].split('-')[1].trim()
-                                },
-                                location: row[4].trim()
-                            }
-
-                            /**
-                             * Evita añadir al horario más de una entrada con lso mismos datos
-                             */
-                            if (!grp_det.schedule.some(item => {
-                                return (item.day == new_item.day &&
-                                    item.interval.end == new_item.interval.end &&
-                                    item.location == new_item.location)
-                            })) {
-                                grp_det.schedule.push(new_item)
-                            }
+                        const $ = cheerio.load(html)
+                        const det = {
+                            subject_name: $('body > div > p.msg1').text(),
+                            dep_name: $('body > div > p:nth-child(2)').text(),
+                            det_asign: $('body > div > p:nth-child(3)').text().split('\n\t\t\t'),
+                            cupos: $('body > div > p:nth-child(4)').text().split('\n\t\t\t'),
+                            professors: $('body > div > p.msg5').text().trim().split(':')[1].split('\n\t'),
+                            dates: $('body > div > p.msg3').text().split('\n')
                         }
-                    })
 
-                    res.statusCode = 200,
+                        let grp_det = {
+                            name: det.subject_name,
+                            dep_name: det.dep_name.split(':').slice(1).join(' ').trim(),
+                            subject: det.det_asign[0].split(':')[1].trim(),
+                            level: det.det_asign[3].split(':')[1].trim(),
+                            group_number: det.det_asign[1].split(':')[1].trim(),
+                            cupos: {
+                                registered: det.cupos[0].split(':')[1].trim(),
+                                available: det.cupos[1].split(':')[1].trim()
+                            },
+                            professors: [],
+                            dates: {
+                                start: det.dates[1].split(':')[1],
+                                end: det.dates[2].split(':')[1]
+                            },
+                            schedule: []
+                        }
+
+                        det.professors.map(professor => {
+                            grp_det.professors.push({
+                                first_name: professor.trim().split(',')[1].trim(),
+                                last_name: professor.trim().split(',')[0].trim()
+                            })
+                        })
+
+                        $('#acreditaciones_resultado > div > div > table > tbody tr').each((i, elem) => {
+                            if (i != 0) {
+                                const row = $(elem).text().trim().split('\n')
+                                let new_item = {
+                                    day: row[2].trim(),
+                                    interval: {
+                                        start: row[3].split('-')[0].trim(),
+                                        end: row[3].split('-')[1].trim()
+                                    },
+                                    location: row[4].trim()
+                                }
+
+                                /**
+                                 * Evita añadir al horario más de una entrada con lso mismos datos
+                                 */
+                                if (!grp_det.schedule.some(item => {
+                                    return (item.day == new_item.day &&
+                                        item.interval.end == new_item.interval.end &&
+                                        item.location == new_item.location)
+                                })) {
+                                    grp_det.schedule.push(new_item)
+                                }
+                            }
+                        })
+
+                        res.statusCode = 200,
                         res.setHeader('Content-type', 'application/json; charset=utf-8')
-                    res.end(JSON.stringify(grp_det))
-                    return
+                        res.end(JSON.stringify(grp_det))
+                        return
+                    } catch (error) {
+                        console.error(error)
+                        somethingWentWrong(res)
+                        return
+                    }
                 })
             })
 
--- routes/grupos_subj_det.js
+++ routes/grupos_subj_det.js
@@ -57,79 +57,85 @@ exports.grupos_subj_det = {
 
                     const $ = cheerio.load(html)
                     let grp = {}
-                    $('body > div').each((index, elem) => {
-                        const det = {
-                            subject_name: $('p.msg1', elem).text(),
-                            dep_name: $('p:nth-child(2)', elem).text(),
-                            det_asign: $('p:nth-child(3)', elem).text().split('\n\t\t\t'),
-                            cupos: $('p:nth-child(6)', elem).text().split('\n\t\t\t'),
-                            dates: $('p.msg3', elem).text().split('\n')
-                        }
-
-                        grp[index] = {
-                            name: det.subject_name,
-                            dep_name: det.dep_name.split(':').slice(1).join(' ').trim(),
-                            subject: det.det_asign[0].split(':')[1].trim(),
-                            group_number: det.det_asign[1].split(':')[1].trim(),
-                            level: $(elem).contents().not($(elem).children()).text().trim(),
-                            nrc: det.det_asign[2].split(':')[1].trim(),
-                            cupos: {
-                                registered: det.cupos[0].split(':')[1].trim(),
-                                available: det.cupos[1].split(':')[1].trim()
-                            },
-                            professors: [],
-                            dates: {
-                                start: det.dates[1].split(':')[1],
-                                end: det.dates[2].split(':')[1]
-                            },
-                            schedule: []
-                        }
-
-                        $('#acreditaciones_resultado > div > div > table > tbody tr', elem).each((i, tr) => {
-                            if (i != 0) {
-                                const row = $(tr).text().trim().split('\n')
-                                let new_item = {
-                                    day: row[2].trim(),
-                                    interval: {
-                                        start: row[3].split('-')[0].trim(),
-                                        end: row[3].split('-')[1].trim()
-                                    },
-                                    location: row[4].trim() 
-                                }
-
-                                /**
-                                 * Verifica que no exista otra entrada en el horario con los mismos dados
-                                 * de la nueva entrada
-                                 */
-                                if (!grp[index].schedule.some(item => {
-                                    return (item.day == new_item.day &&
-                                        item.interval.end == new_item.interval.end &&
-                                        item.location == new_item.location)
-                                })){
-                                    grp[index].schedule.push(new_item)
-                                }
-
-                                let professor = {
-                                    first_name: row[5].split(',')[1].trim(),
-                                    last_name: row[5].split(',')[0].trim()
-                                }
-
-                                /**
-                                 * Evita que se añada el mismo profesor más de una vez
-                                 */
-                                if(!grp[index].professors.some(p => {
-                                    return (p.first_name == professor.first_name && p.last_name == professor.last_name)
-                                })){
-                                    grp[index].professors.push(professor)
-                                }
+                    try {
+                        $('body > div').each((index, elem) => {
+                            const det = {
+                                subject_name: $('p.msg1', elem).text(),
+                                dep_name: $('p:nth-child(2)', elem).text(),
+                                det_asign: $('p:nth-child(3)', elem).text().split('\n\t\t\t'),
+                                cupos: $('p:nth-child(6)', elem).text().split('\n\t\t\t'),
+                                dates: $('p.msg3', elem).text().split('\n')
                             }
-                        })
-                    })
 
-                    res.statusCode = 200
-                    res.setHeader('Content-type', 'application/json; charset=utf-8')
-                    res.end(JSON.stringify(grp))
-                    return
+                            grp[index] = {
+                                name: det.subject_name,
+                                dep_name: det.dep_name.split(':').slice(1).join(' ').trim(),
+                                subject: det.det_asign[0].split(':')[1].trim(),
+                                group_number: det.det_asign[1].split(':')[1].trim(),
+                                level: $(elem).contents().not($(elem).children()).text().trim(),
+                                nrc: det.det_asign[2].split(':')[1].trim(),
+                                cupos: {
+                                    registered: det.cupos[0].split(':')[1].trim(),
+                                    available: det.cupos[1].split(':')[1].trim()
+                                },
+                                professors: [],
+                                dates: {
+                                    start: det.dates[1].split(':')[1],
+                                    end: det.dates[2].split(':')[1]
+                                },
+                                schedule: []
+                            }
+
+                            $('#acreditaciones_resultado > div > div > table > tbody tr', elem).each((i, tr) => {
+                                if (i != 0) {
+                                    const row = $(tr).text().trim().split('\n')
+                                    let new_item = {
+                                        day: row[2].trim(),
+                                        interval: {
+                                            start: row[3].split('-')[0].trim(),
+                                            end: row[3].split('-')[1].trim()
+                                        },
+                                        location: row[4].trim()
+                                    }
+
+                                    /**
+                                     * Verifica que no exista otra entrada en el horario con los mismos dados
+                                     * de la nueva entrada
+                                     */
+                                    if (!grp[index].schedule.some(item => {
+                                        return (item.day == new_item.day &&
+                                            item.interval.end == new_item.interval.end &&
+                                            item.location == new_item.location)
+                                    })) {
+                                        grp[index].schedule.push(new_item)
+                                    }
+
+                                    let professor = {
+                                        first_name: row[5].split(',')[1].trim(),
+                                        last_name: row[5].split(',')[0].trim()
+                                    }
+
+                                    /**
+                                     * Evita que se añada el mismo profesor más de una vez
+                                     */
+                                    if (!grp[index].professors.some(p => {
+                                        return (p.first_name == professor.first_name && p.last_name == professor.last_name)
+                                    })) {
+                                        grp[index].professors.push(professor)
+                                    }
+                                }
+                            })
+                        })
+
+                        res.statusCode = 200
+                        res.setHeader('Content-type', 'application/json; charset=utf-8')
+                        res.end(JSON.stringify(grp))
+                        return
+                    } catch (error) {
+                        console.error(error)
+                        somethingWentWrong(res)
+                        return
+                    }
                 })
             })
 
